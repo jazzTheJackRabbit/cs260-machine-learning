6ece521c3f13ce78bdeee58e36ba1f0e9d1ed8e8
jazzTheJackRabbit <amogh.91@gmail.com>
2015-03-08 17:46:18 -0700

    Data preparation and Patient Class

---
+++ src/Patient.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+class Patient:
+	def __init__(self,filename):
+		
+		#measurements
+		self.diastolicMeasurements = np.zeros([26,1])
+		self.systolicMeasurements = np.zeros([26,1])
+		
+		#properties
+		self.filename = ""
+		self.classificationLabel = 0
+		self.patientID = 0
+		
+		#features
+		self.meanOfMeasurements = (0,0)
+		self.varianceOfMeasurements = (0,0)
+		self.standardDeviation = (0,0)
+		
+		self.parseFileName(filename)
+		
+	def parseFileName(self,filename):
+		self.filename = filename
+		self.patientID = filename.split("/").pop().split(".")[0].split("_")[0]
+		self.classificationLabel = filename.split("/").pop().split(".")[0].split("_")[1]
+	
+	def computeMean(self):
+		meanForDiastolic = np.dot(np.transpose(self.diastolicMeasurements),np.ones([len(self.diastolicMeasurements),1]))[0,0]/len(self.diastolicMeasurements)
+		meanForSystolic = np.dot(np.transpose(self.systolicMeasurements),np.ones([len(self.systolicMeasurements),1]))[0,0]/len(self.systolicMeasurements)
+		self.meanOfMeasurements = (meanForDiastolic,meanForSystolic) 
+		return self.meanOfMeasurements
+	
+	def computeVariance(self):
+		matrixOfDifferencesForDiastolicMeasurements = np.subtract(self.diastolicMeasurements,self.meanOfMeasurements[0])
+		matrixOfDifferencesForSystolicMeasurements = np.subtract(self.systolicMeasurements,self.meanOfMeasurements[1])
+		varianceForDiastolicMeasurements = np.dot(np.transpose(matrixOfDifferencesForDiastolicMeasurements),matrixOfDifferencesForDiastolicMeasurements)[0,0]
+		varianceForSystolicMeasurements = np.dot(np.transpose(matrixOfDifferencesForSystolicMeasurements),matrixOfDifferencesForSystolicMeasurements)[0,0]		
+		
+		self.varianceOfMeasurements = (varianceForDiastolicMeasurements,varianceForSystolicMeasurements)				
+		self.standardDeviation = (self.varianceOfMeasurements[0]**(0.5),self.varianceOfMeasurements[1]**(0.5))			
+		
+	def printMeasurements(self):
+		print "\n##################\nDiastolic Measurements for Patient#"+str(self.patientID)+"\n##################\n"
+		print self.diastolicMeasurements
+		print "\n##################\nSystolic Measurements for Patient#"+str(self.patientID)+"\n##################\n"
+		print self.systolicMeasurements
--- src/dataPreparation.py
+++ src/dataPreparation.py
@@ -1,35 +1,74 @@
+from Patient import Patient 
 import os
 import numpy as np
 
-allFilePathList = []
-
-for dirname, dirnames, filenames in os.walk('/Users/amogh/workspace/jazz/ucla/cs260a/MachineLearningProject/dataset/outDataClass'):
-    # print path to all filenames.
-    for filename in filenames:
-        if(filename != ".DS_Store"):
-            allFilePathList.append(os.path.join(dirname, filename))
-
-allPatientData = []
-for patientIndex in range(len(allFilePathList)):    
-    filePath = allFilePathList[patientIndex]
-    patientFileObject = open(filePath,'r')
-    line = patientFileObject.readline()
-    fileContents = []
-    patientData = np.matrix([0,0])
-    while(line):
-        fileContents.append(line)
-        line = patientFileObject.readline()
-    fileContents = fileContents[1:]
-    for patientRowIndex in range(len(fileContents)):
-        rowString = fileContents[patientRowIndex].split("\n")[0].split(",")
-        row = np.matrix([int(rowString[1]),int(rowString[2])])
-        if(patientRowIndex!=0):
-            patientData = np.append(patientData,row,axis=0)
-        else:
-            patientData = row             
-            
-    allPatientData.append(patientData)
+def createPatientsInDirectory(directory):    
+    allPatients = []    
+    for dirname, dirnames, filenames in os.walk(directory):
+        # print path to all filenames except .DS_Store.
+        for filename in filenames:
+            if(filename != ".DS_Store"):                
+                patient = Patient(os.path.join(dirname, filename))
+                allPatients.append(patient)
                 
-print allPatientData
+    return allPatients
+    
+def readPatientDataForFilesInDirectory(directory):    
+    allPatients = createPatientsInDirectory(directory)
+    
+    for patientIndex in range(len(allPatients)):    
+        filePath = allPatients[patientIndex].filename
+        patientFileObject = open(filePath,'r')
+        line = patientFileObject.readline()
+        fileContents = []
+        patientData = np.matrix([0,0])
+        
+        #Read all lines of the file.
+        while(line):
+            fileContents.append(line)
+            line = patientFileObject.readline()
+        
+        #Remove title header.
+        fileContents = fileContents[1:]
+        for patientRowIndex in range(len(fileContents)):
+            rowString = fileContents[patientRowIndex].split("\n")[0].split(",")
+            row = np.matrix([int(rowString[1]),int(rowString[2])])
+            if(patientRowIndex!=0):
+                patientData = np.append(patientData,row,axis=0)
+            else:
+                patientData = row             
+                                
+        allPatients[patientIndex].diastolicMeasurements = patientData[:,[0]]
+        allPatients[patientIndex].systolicMeasurements = patientData[:,[1]]
+                                    
+    return allPatients
+    
+#Main
+allPatients = readPatientDataForFilesInDirectory('/Users/amogh/workspace/jazz/ucla/cs260a/MachineLearningProject/dataset/outDataClass')
+
+print "\n##################\n Labels\n##################\n"
+for patientIndex in range(len(allPatients)):     
+    print allPatients[patientIndex].classificationLabel 
+
+print "\n##################\n Compute Mean and Variance\n##################\n"
+for patientIndex in range(len(allPatients)):    
+    meanForPatientMeasurements = allPatients[patientIndex].computeMean()
+    varianceOfMeasurements = allPatients[patientIndex].computeVariance()
+
+print "\n##################\n Diastolic Mean\n##################\n"
+for patientIndex in range(len(allPatients)):
+    print allPatients[patientIndex].meanOfMeasurements[0]
+
+print "\n##################\n Systolic Mean\n##################\n"
+for patientIndex in range(len(allPatients)):
+    print allPatients[patientIndex].meanOfMeasurements[1]
+
+print "\n##################\n Diastolic Variance\n##################\n"
+for patientIndex in range(len(allPatients)):
+    print allPatients[patientIndex].standardDeviation[0]/allPatients[patientIndex].meanOfMeasurements[0]
+
+print "\n##################\n Systolic Variance\n##################\n"
+for patientIndex in range(len(allPatients)):
+    print allPatients[patientIndex].standardDeviation[1]/allPatients[patientIndex].meanOfMeasurements[1]
 
         
